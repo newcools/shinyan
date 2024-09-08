776f2f554cb57abae436daec937dd86fd7828f2f
Sam Peng <sam@minjet.com.au>
2024-09-08 15:17:44 +1000

    a fix

--- iac/modules/cosmosdb/main.tf
+++ iac/modules/cosmosdb/main.tf
@@ -8,29 +8,39 @@ resource "azurerm_cosmosdb_account" "cosmosdb_serverless" {
   resource_group_name = var.resource_group_name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
-  enable_free_tier    = true
+  free_tier_enabled    = true
   capabilities {
     name = "EnableServerless"
+  }
+
+  # Add geo_location block
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+
+  consistency_policy {
+    consistency_level = "Session"
   }
 }
 
 resource "azurerm_cosmosdb_sql_database" "database" {
   name                = var.database_name
-  cosmosdb_account_id = azurerm_cosmosdb_account.cosmosdb_serverless.id
+  account_name        = var.cosmosdb_account_name
   resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_cosmosdb_sql_container" "container" {
   name                = var.container_name
-  cosmosdb_account_id = azurerm_cosmosdb_account.cosmosdb_serverless.id
+  account_name        = var.cosmosdb_account_name
   resource_group_name = var.resource_group_name
   database_name       = azurerm_cosmosdb_sql_database.database.name
-  partition_key_path  = var.partition_key_path
+  partition_key_paths   = [var.partition_key_path]
 }
 
 # Assign the role to the Developers group for Cosmos DB
 resource "azurerm_role_assignment" "developers_cosmosdb_role_assignment" {
-  principal_id        = var.principal_id  # Accept the group ID as input to the module
+  principal_id        = var.principal_id
   role_definition_name = "Cosmos DB Built-in Data Contributor"
   scope               = azurerm_cosmosdb_account.cosmosdb_serverless.id
 }
